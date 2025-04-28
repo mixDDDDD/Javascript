2c4c03e8a702d6463582ed2febb51f584b417469
Дмитрий <yhoooo39@gmail.com>
2025-04-28 17:31:39 +0300

    Улучшения кода

--- 12-luna/12-luna.js
+++ 12-luna/12-luna.js
@@ -1,3 +1,4 @@
+// Мой вариант
 const card = "4561-1213-4367-2612";
 
 function Luna(cardNumber) {
@@ -18,3 +19,46 @@ function Luna(cardNumber) {
 }
 
 console.log(Luna(card));
+
+// Вариант наставника
+function getRandomNumber(min = 0, max = 9) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateArray(fn, count) {
+    return Array.from({ length: count }, () => fn());
+}
+
+function generateCardPart(fn, count) {
+    return generateArray(fn, count).join('');
+}
+
+function getRandomCardNumber() {
+    const cardNumber = generateArray(() => generateCardPart(getRandomNumber, 4), 4);
+    return cardNumber.join('-');
+}
+
+function cardLunaValidate(card) {
+    const cardNumber = card.replaceAll('-', '').split('').map(Number);
+    if (cardNumber.some(isNaN)) return false;
+    let sum = 0;
+    for (let i = 0; i < cardNumber.length; i++) {
+        let digit = cardNumber[i];
+        if (i % 2 === 0) {
+            digit = digit * 2 > 9 ? digit * 2 - 9 : digit * 2;
+        }
+        sum += digit;
+    }
+    return sum % 10 === 0;
+}
+
+function resultTemplate(card) {
+    return `Карта с номером: ${card} получила результат: ${cardLunaValidate(card)}`;
+}
+
+const cardArray = Array.from({ length: getRandomNumber(5, 10) }, () => getRandomCardNumber());
+const resultArray = cardArray.map(resultTemplate);
+
+for (const result of resultArray) {
+    console.log(result);
+}
